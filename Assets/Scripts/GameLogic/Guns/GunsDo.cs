938f2faa100667f65753119e914841c8d53bee74
Moroxso <c.kuzickoff@yandex.ru>
2025-03-28 13:45:13 +0400

    Система отдачи есть!

--- Assets/Scripts/GameLogic/Guns/GunsDo.cs
+++ Assets/Scripts/GameLogic/Guns/GunsDo.cs
@@ -7,104 +7,49 @@ using System;
 public class GunsDo : MonoBehaviour
 {
     [SerializeField] GameObject Gun;
-    private Transform position1;
-    private Transform position2;
+    [SerializeField] float returnSpeed = 5f;
+    private Quaternion initialRotation;
 
     void Start()
     {
-        position1 = Gun.transform;
-        position2 = position1;
+        initialRotation = Gun.transform.localRotation;
     }
-
 
     void Update()
     {
-        
+        // Плавно возвращаем оружие в исходное положение
+        if (Gun.transform.localRotation != initialRotation)
+        {
+            Gun.transform.localRotation = Quaternion.Lerp(
+                Gun.transform.localRotation,
+                initialRotation,
+                Time.deltaTime * returnSpeed
+            );
+        }
     }
 
     public void blowback()
     {
-        float zRotation;
-        float yRotation;
-        float xRotation;
         System.Random rand = new System.Random();
-
         int a = rand.Next(0, 9);
+
+        Vector3 recoilVector = Vector3.zero;
 
         switch (a)
         {
-            case 0:
-                zRotation = Gun.transform.localRotation.z - 0.1f;
-                yRotation = Gun.transform.localRotation.y + 1f;
-                xRotation = Gun.transform.localRotation.x;
-                Gun.transform.localRotation = Quaternion.Euler(xRotation, yRotation, zRotation);
-                break;
-            case 1:
-                zRotation = Gun.transform.localRotation.z - 0.2f;
-                yRotation = Gun.transform.localRotation.y - 1f;
-                xRotation = Gun.transform.localRotation.x;
-                Gun.transform.localRotation = Quaternion.Euler(xRotation, yRotation, zRotation);
-                break;
-            case 2:
-                zRotation = Gun.transform.localRotation.z - 0.3f;
-                yRotation = Gun.transform.localRotation.y + 2f;
-                xRotation = Gun.transform.localRotation.x;
-                Gun.transform.localRotation = Quaternion.Euler(xRotation, yRotation, zRotation);
-                break;
-            case 3:
-                zRotation = Gun.transform.localRotation.z - 0.4f;
-                yRotation = Gun.transform.localRotation.y - 2f;
-                xRotation = Gun.transform.localRotation.x;
-                Gun.transform.localRotation = Quaternion.Euler(xRotation, yRotation, zRotation);
-                break;
-            case 4:
-                zRotation = Gun.transform.localRotation.z - 0.5f;
-                yRotation = Gun.transform.localRotation.y + 1.5f;
-                xRotation = Gun.transform.localRotation.x;
-                Gun.transform.localRotation = Quaternion.Euler(xRotation, yRotation, zRotation);
-                break;
-            case 5:
-                zRotation = Gun.transform.localRotation.z - 0.6f;
-                yRotation = Gun.transform.localRotation.y - 1.5f;
-                xRotation = Gun.transform.localRotation.x;
-                Gun.transform.localRotation = Quaternion.Euler(xRotation, yRotation, zRotation);
-                break;
-            case 6:
-                zRotation = Gun.transform.localRotation.z - 0.7f;
-                yRotation = Gun.transform.localRotation.y + 0.5f;
-                xRotation = Gun.transform.localRotation.x;
-                Gun.transform.localRotation = Quaternion.Euler(xRotation, yRotation, zRotation);
-                break;
-            case 7:
-                zRotation = Gun.transform.localRotation.z - 0.8f;
-                yRotation = Gun.transform.localRotation.y - 0.5f;
-                xRotation = Gun.transform.localRotation.x;
-                Gun.transform.localRotation = Quaternion.Euler(xRotation, yRotation, zRotation);
-                break;
-            case 8:
-                zRotation = Gun.transform.localRotation.z - 0.9f;
-                yRotation = Gun.transform.localRotation.y + 0.25f;
-                xRotation = Gun.transform.localRotation.x;
-                Gun.transform.localRotation = Quaternion.Euler(xRotation, yRotation, zRotation);
-                break;
-            case 9:
-                zRotation = Gun.transform.localRotation.z - 1.0f;
-                yRotation = Gun.transform.localRotation.y - 0.25f;
-                xRotation = Gun.transform.localRotation.x;
-                Gun.transform.localRotation = Quaternion.Euler(xRotation, yRotation, zRotation);
-                break;
+            case 0: recoilVector = new Vector3(0, 1f, -0.1f); break;
+            case 1: recoilVector = new Vector3(0, -1f, -0.2f); break;
+            case 2: recoilVector = new Vector3(0, 2f, -0.3f); break;
+            case 3: recoilVector = new Vector3(0, -2f, -0.4f); break;
+            case 4: recoilVector = new Vector3(0, 1.5f, -0.5f); break;
+            case 5: recoilVector = new Vector3(0, -1.5f, -0.6f); break;
+            case 6: recoilVector = new Vector3(0, 0.5f, -0.7f); break;
+            case 7: recoilVector = new Vector3(0, -0.5f, -0.8f); break;
+            case 8: recoilVector = new Vector3(0, 0.25f, -0.9f); break;
+            case 9: recoilVector = new Vector3(0, -0.25f, -1.0f); break;
         }
+
+        // Применяем отдачу к начальному повороту
+        Gun.transform.localRotation = initialRotation * Quaternion.Euler(recoilVector);
     }
-
-
-
-
-
-
-
-
-
-
-
-
 }
